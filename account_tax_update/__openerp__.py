61da4f89b9abc8d436a8d661f8590086d28f6409
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-07-06 11:38:42 +0200

    [WIP]

---
+++ account_tax_update/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'account_tax_update TO DELETE',
+    'version': '8.0.1.0.0',
+    'category': 'useless',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+}
