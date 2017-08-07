576dcbd0b6a6bfe3a949c8cf9088126354b2b12e
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-08-07 14:22:21 +0200

    wip

--- sale_visible_tax/__openerp__.py
+++ sale_visible_tax/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'account_delete_move_null_amount TO DELETE',
+    'name': 'sale_visible_tax TO DELETE',
     'version': '8.0.1.0.0',
     'category': 'useless',
     'author': 'GRAP',
