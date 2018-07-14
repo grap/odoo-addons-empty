d7c574f566e150c857a60a41f088ed8477368271
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-14 19:32:57 +0200

    [REF] restore obsolete module

---
+++ grap_openupgrade_to_8/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'grap_openupgrade_to_8 TO DELETE',
+    'version': '8.0.1.0.0',
+    'category': 'useless',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+}
